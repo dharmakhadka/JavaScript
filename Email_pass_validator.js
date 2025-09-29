bb80e40379020b48adb025c1ec8fd9defa7c73c0
Dharma_KhadkaChhetri <dharma.khadka.315@gmail.com>
2025-09-29 07:24:32 +0545

    ,

--- Email_pass_validator.js
+++ Email_pass_validator.js
@@ -1,10 +1,20 @@
-let form = document.querySelector("submit");
 let email = document.querySelector("#email");
 let pass = document.querySelector("#password"); 
+let form = document.querySelector("form");
+
 
 form.addEventListener("submit",function(dets){
     dets.preventDefault();
     
-    
+    const email_regx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/i
+    const pass_regx = /^(?=.[A-Za-z])(?=.\d)[A-Za-z\d]{8,}$/;
+
+    let email_c = email_regx.test(email.value);
+    let pass_c = pass_regx.test(pass.value);
+
+    if(!email_c){
+        document.querySelector("#error_text").style.display = "initial";
+        document.addEventListener("#error_text").textContent = "Error Email";
+    }
 
 })

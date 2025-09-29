e4656e8c67a63a8f25391664395692098383a328
Dharma_KhadkaChhetri <dharma.khadka.315@gmail.com>
2025-09-29 07:36:47 +0545

    email/pass error requirement check

--- Email_pass_validator.js
+++ Email_pass_validator.js
@@ -7,14 +7,18 @@ form.addEventListener("submit",function(dets){
     dets.preventDefault();
     
     const email_regx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/i
-    const pass_regx = /^(?=.[A-Za-z])(?=.\d)[A-Za-z\d]{8,}$/;
+    const pass_regx = /^.{8,}$/;
 
     let email_c = email_regx.test(email.value);
     let pass_c = pass_regx.test(pass.value);
 
     if(!email_c){
-        document.querySelector("#error_text").style.display = "initial";
-        document.addEventListener("#error_text").textContent = "Error Email";
+        document.querySelector("#error_email").style.display = "initial";
+        document.querySelector("#error_email").textContent = "Error Email";
+    }
+    if(!pass_c){
+        document.querySelector("#error_password").style.display = "initial";
+        document.querySelector("#error_password").textContent = "Error Password";
     }
 
 })

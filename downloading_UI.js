a702116afe76dd1bda25678ee9b27e66ff30ac93
Dharma_KhadkaChhetri <dharmakhadkachhetri@gmail.com>
2025-10-01 19:41:44 +0545

    update

---
+++ downloading_UI.js
@@ -0,0 +1,12 @@
+let count = 0;
+let progress = document.querySelector(".bar");
+let progressText = document.querySelector("#percentText");
+
+setInterval(function(){
+    if(count < 99){
+        count++;
+        progress.style.width = `${count}%`;
+        progressText.textContent = `${count}%`;
+        
+    }
+}, 500);
